da6f7ecbe0c52a319f2c59c27fe6948c80bee279
Sanjay_Balaji <70127280+sanjay-dot@users.noreply.github.com>
2021-10-21 18:05:47 +0530

    Add files via upload

--- forms.php
+++ forms.php
@@ -5,31 +5,50 @@ session_start();
    $model =trim($_POST['model']);
    $name =trim($_POST['name']);
    $price =trim($_POST['price']);
-//    $file = $_FILES["name"]["file"];
-//    $tmp_file = $_FILES["tmp_name"]["file"];
 
-//    $path = "uploads/".$file;
-//     move_uploaded_file($file, $path);
-  if(isset($_SESSION["phone"]))
-  {
-    array_push($_SESSION['phone'],array("Model"=>$model, "Name"=>$name,"Price"=>$price));
-  }
-  else{
-    $_SESSION['phone'] = array();
-    array_push($_SESSION['phone'],array("Model"=>$model, "Name"=>$name,"Price"=>$price));
+   $Image = $_FILES['file'];
+   $ImageName = $_FILES['file']['name'];
+   $error =  $_FILES['file']['error'];
 
-  }
+   $tempExt = explode('.',$ImageName);
+   $fileExt = strtolower(end($tempExt));
+   $isallow = array('jpg','jpeg','png');
+
+  if(in_array($fileExt, $isallow)){
+    if($error === 0){
+        $path = '/var/www/html/Project/uploads/'; 
+        $location = $path . $_FILES['file']['name']; 
+        move_uploaded_file($_FILES['file']['tmp_name'], $location); 
+    }
 
     if(isset($_SESSION["phone"]))
     {
-        // print_r($_SESSION["phone"]);
-        echo json_encode($_SESSION["phone"]);  
+      array_push($_SESSION['phone'],array("Model"=>$model, "Name"=>$name,"Price"=>$price,"filename"=>$ImageName));
     }
-    else
-    {
-        echo"Fail";
+    else{
+      $_SESSION['phone'] = array();
+      array_push($_SESSION['phone'],array("Model"=>$model, "Name"=>$name,"Price"=>$price,"filename"=>$ImageName));
+
     }
-    
+
+      if(isset($_SESSION["phone"]))
+      {
+          // print_r($_SESSION["phone"]);
+          echo json_encode($_SESSION["phone"]);  
+      }
+      else
+      {
+          echo"Fail";
+      }
+      
+    }
+    else{
+      $error = 2;
+      echo $error;
+    }
+
+
+
    
 ?>  
  
